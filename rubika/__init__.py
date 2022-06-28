38bccc086e7748f0f22c4a5ae77fff429be2b08d
Milad Heidari <61160040+IRMilad@users.noreply.github.com>
2022-06-27 18:19:39 -0700

    Delete rubika directory

--- rubika/__init__.py
+++
@@ -1,4 +0,0 @@
-from .app import Client
-from .network import Proxies
-from .gadgets import events, errors, methods
-__version__ = '1.0.0'
--- rubika/gadgets/__init__.py
+++
@@ -1,5 +0,0 @@
-
-from . import errors
-from . import events
-from . import methods
-from .updateStruct import UpdateStruct
