c65b6f7b5c3d6fa738bd48ef62c41f3a662d9eab
Milad Heidary <61160040+IRMilad@users.noreply.github.com>
2022-07-03 11:56:34 -0700

    Update errors.py

--- rubika/gadgets/errors.py
+++ rubika/gadgets/errors.py
@@ -1,8 +1,17 @@
+import sys
+
 class ClientError(Exception):
     pass
 
-
 class SocksError(ClientError):
+    pass
+
+
+class StopHandler(ClientError):
+    pass
+
+
+class CancelledError(ClientError):
     pass
 
 
@@ -30,34 +39,105 @@ class LoginAuthenticationFailed(SocksError):
     pass
 
 
-class ConnectionError(ClientError):
-    def __init__(self, message, request=None):
-        self.message = (str(message))
-        self.request = request
-
-
-class NoConnection(ConnectionError):
-    pass
-
-
-class ConnectionUrlNotFound(ConnectionError):
-    pass
-
-
-class ConnectionInternalProblem(ConnectionError):
-    pass
-
-
 class RequestError(ClientError):
-    def __init__(self, message, request=None, det=None) -> None:
+    def __init__(self, message, request=None):
         self.message = str(message)
         self.request = request
-        self.det = det
 
 
-class CancelledError(RequestError):
+class CodeIsUsed(RequestError):
     pass
 
 
-class StopHandler(ClientError):
+class TooRequests(RequestError):
     pass
+
+
+class InvalidAuth(RequestError):
+    pass
+
+
+class ServerError(RequestError):
+    pass
+
+
+class UrlNotFound(RequestError):
+    pass
+
+
+class ErrorAction(RequestError):
+    pass
+
+
+class ErrorIgnore(RequestError):
+    pass
+
+
+class ErrorGeneric(RequestError):
+    pass
+
+
+class NoConnection(RequestError):
+    pass
+
+
+class InvalidInput(RequestError):
+    pass
+
+
+class Undeliverable(RequestError):
+    pass
+
+
+class NotRegistered(RequestError):
+    pass
+
+
+class CodeIsExpired(RequestError):
+    pass
+
+
+class InvalidMethod(RequestError):
+    pass
+
+
+class UsernameExist(RequestError):
+    pass
+
+
+class NotRegistrred(RequestError):
+    pass
+
+
+class ErrorTryAgain(RequestError):
+    pass
+
+
+class ErrorMessageTry(RequestError):
+    pass
+
+
+class InternalProblem(RequestError):
+    pass
+
+
+class ErrorMessageIgn(RequestError):
+    pass
+
+
+class NotSupportedApiVersion(RequestError):
+    pass
+
+
+class ExcetionsHandler:
+    def __init__(self, name) -> None:
+        self.name = name
+    
+    def __getattr__(self, name):
+        name = ''.join([chunk.title() for chunk in name.split('_')])
+        return globals().get(name, ClientError)
+
+    def __call__(self, name, *args, **kwargs):
+        return getattr(self, name)
+
+sys.modules[__name__] = ExcetionsHandler(__name__)
