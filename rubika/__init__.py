e844efef27ade4491b15218001500adfd8e67099
Milad Heidary <61160040+IRMilad@users.noreply.github.com>
2022-07-05 16:43:32 -0700

    Update __init__.py

--- rubika/__init__.py
+++ rubika/__init__.py
@@ -2,4 +2,4 @@ from .client import Client
 from .network import Proxies
 from .structs import handlers, models
 from .gadgets import exceptions, methods
-__version__ = 'beta-22.7.3'
+__version__ = 'beta-22.7.6'
