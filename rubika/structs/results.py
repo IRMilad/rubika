aa956490e73589df08caf5bd29b8ae254399aa26
Milad Heidary <61160040+IRMilad@users.noreply.github.com>
2022-07-05 16:29:44 -0700

    Update results.py

--- rubika/structs/results.py
+++ rubika/structs/results.py
@@ -2,7 +2,6 @@ import sys
 from .struct import Struct
 from ..gadgets import Classino
 
-__results__ = []
 
 class BaseResults(Struct):
     __type__ = 'CustomResult'
@@ -14,17 +13,15 @@ class BaseResults(Struct):
 class Results(Classino):
     def __init__(self, name, *args, **kwargs) -> None:
         self.__name__ = name
-    
+
     def __eq__(self, value: object) -> bool:
         return BaseResults in value.__bases__
-
-    def __dir__(self):
-        return sorted(__results__)
 
     def __call__(self, name, *args, **kwargs):
         return self.__getattr__(name)(*args, **kwargs)
 
     def __getattr__(self, name):
         return self.create(name, (BaseResults, ), exception=False)
+
 
 sys.modules[__name__] = Results(__name__)
