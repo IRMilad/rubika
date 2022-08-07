918fdc13690b1c5761882d35b2578b4c7f8dae44
Milad Heidary <61160040+IRMilad@users.noreply.github.com>
2022-08-07 18:17:30 +0430

    Update thumbnail.py

--- rubika/gadgets/thumbnail.py
+++ rubika/gadgets/thumbnail.py
@@ -38,7 +38,7 @@ class MakeThumbnail(Thumbnail):
                  width: int = 200,
                  height: int = 200,
                  seconds: int = 1, *args, **kwargs) -> None:
-        self.image = b''
+        self.image = None
         self.width = width
         self.height = height
         self.seconds = seconds
